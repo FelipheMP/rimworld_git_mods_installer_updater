44fef16e9d14436e363f155ae6f6234555c8c41e
Feliphe M. Pereira <36012629+FelipheMP@users.noreply.github.com>
2025-04-16 22:44:45 -0300

    Update scripts to use main.py for mod updates

--- konsole_launch_mod_updater.sh
+++ konsole_launch_mod_updater.sh
@@ -4,18 +4,18 @@
 # Adjust arguments values after the script's name
 
 # Updating from the first mod to 37th
-konsole -e python rimworld_git_mods_installer_updater.py 0 37 &
+konsole -e python main.py 0 37 &
 
 # Updating more 37 mods = (0 + 37 = 37)
-konsole -e python rimworld_git_mods_installer_updater.py 37 37 &
+konsole -e python main.py 37 37 &
 
 # Updating more 36 mods = (37 + 36 = 74)
-konsole -e python rimworld_git_mods_installer_updater.py 74 36 &
+konsole -e python main.py 74 36 &
 
 # Updating more 36 mods = (74 + 36 = 110)
-konsole -e python rimworld_git_mods_installer_updater.py 110 36 &
+konsole -e python main.py 110 36 &
 
 # Updating last 37 mods = (110 + 37 = 146)
-konsole -e python rimworld_git_mods_installer_updater.py 146 37 &
+konsole -e python main.py 146 37 &
 
 # Total = 183 Mods! Yes, I have quite a bunch of mods installed... xD
--- launch_mod_updater.bat
+++ launch_mod_updater.bat
@@ -5,18 +5,18 @@
 @echo off
 
 :: Updating from the first mod to 37th
-start cmd /k python rimworld_git_mods_installer_updater.py 0 37
+start cmd /k python main.py 0 37
 
 :: Updating more 37 mods = (0 + 37 = 37)
-start cmd /k python rimworld_git_mods_installer_updater.py 37 37
+start cmd /k python main.py 37 37
 
 :: Updating more 36 mods = (37 + 36 = 74)
-start cmd /k python rimworld_git_mods_installer_updater.py 74 36
+start cmd /k python main.py 74 36
 
 :: Updating more 36 mods = (74 + 36 = 110)
-start cmd /k python rimworld_git_mods_installer_updater.py 110 36
+start cmd /k python main.py 110 36
 
 :: Updating last 37 mods = (110 + 37 = 146)
-start cmd /k python rimworld_git_mods_installer_updater.py 146 37
+start cmd /k python main.py 146 37
 
 :: Total = 183 Mods! Yes, I have quite a bunch of mods installed... xD
